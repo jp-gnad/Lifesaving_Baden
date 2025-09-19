c53b28fe9f6dc257313bc73a8f1dcdfb6fe4d130
Jan-Philipp Gnad <jp.gnad@web.de>
2025-09-19 09:46:24 +0200

    Update athleten.js

--- athleten.js
+++ athleten.js
@@ -157,9 +157,9 @@ function filterZeit_50retten(eintrag, kaderArray) {
 
   let richtzeit_50retten;
   if (eintrag.geschlecht === "m") {
-    richtzeit_50retten = kaderArray[2][0];
-  } else if (eintrag.geschlecht === "w") {
     richtzeit_50retten = kaderArray[3][0];
+  } else if (eintrag.geschlecht === "w") {
+    richtzeit_50retten = kaderArray[2][0];
   } else {
     return false;
   }
@@ -232,6 +232,7 @@ document.addEventListener("DOMContentLoaded", () => {
     ladeAthleten().catch(err => console.error("Fehler beim Laden der Excel:", err));
   });
 });
+
 
 
 
