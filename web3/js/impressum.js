ad3b647ece365ccd431e16bfe98713cd6604cd67
Jan-Philipp Gnad <jp.gnad@web.de>
2025-11-13 01:12:03 +0100

    Logo für Website?

--- web3/js/impressum.js
+++ web3/js/impressum.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
         <h2>Angaben gemäß § 5 TMG</h2>
         <p>
           <strong>Name:</strong> <span class="placeholder">Jan-Philipp Gnad</span><br>
-          <strong>Adresse:</strong> <span class="placeholder">Am Zollstock 36</span><br>
+          <strong>Adresse:</strong> <span class="placeholder">Am Zollstock 36, 76228 Karlsuhe</span><br>
           <strong>E-Mail:</strong> <span class="placeholder">jan-philipp.gnad@dlrg.org</span><br>
           <strong>Telefon:</strong> <span class="placeholder">+49 (0)1575 4047493</span>
         </p>
